465a43aabbf8d4ac2ab56a2e9bca52bd01c07535
Shindu Astawa <shinduastawa@gmail.com>
2024-10-08 20:25:46 +0800

    penghapusan import dist folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import './dist/css/main.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './Api/contexts/AuthContext.jsx';  // Import AuthProvider
 
